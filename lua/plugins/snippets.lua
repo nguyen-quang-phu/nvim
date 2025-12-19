2ce786d5d238c062a0700206e4108e5b713d5671
keynold <nqphu1998@gmail.com>
2025-12-19 18:22:27 +0700

    ✨ feat: add snippets plugins

---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "chrisgrieser/nvim-scissors",
+    dependencies = {
+    }, -- optional
+    opts = {
+      snippetDir = "~/.config/nvim/snippets",
+      jsonFormatter = "jq",
+      snippetSelection = {
+        picker = "snacks", ---@type "auto"|"telescope"|"snacks"|"vim.ui.select"
+      },
+    },
+  },
+  {
+    "DimitrisDimitropoulos/yasp.nvim",
+    -- lazy loading is not required, since it is handled internally
+    lazy = false,
+    -- if you persist on lazy loading you must call the setup function on InsertEnter
+    -- event = 'InsertEnter',
+    opts = {
+      -- default, change to false for special completion frameworks
+      -- long_desc = true,
+      -- default, change to true mainly for debugging
+      -- prose = false,
+      -- default, the time to wait before starting a new server in milliseconds, highly suggested to keep it
+      -- debounce = 750,
+      -- default, global triggerChars to fire lsp completion
+      -- trigger_chars = {
+      --   ['*'] = { '{', '(', '[', ' ', '.', ':', ',' },
+      --   -- append them per filetype in such style
+      --   -- ['lua'] = { '.', ':' },
+      -- },
+
+      -- 💀 WARNING: the following must be provided by the user
+      -- the paths to the package.json files, no default given, must be provided
+      paths = {
+        "/Users/dev/.config/nvim/snippets/package.json"
+      },
+      descs = { "keynold snippets" },
+    },
+  },
+}
